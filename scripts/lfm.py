8e346f1417eb80cfbe1987a0cf19b1e993c25722
Georg Eschbacher <geschbacher.mmt-m2014@fh-salzburg.ac.at>
2015-11-13 15:09:22 +0100

    updating gitignore and refactoring lfm wrapper

--- scripts/lfm.py
+++ scripts/lfm.py
@@ -1,6 +1,4 @@
 import urllib
-import pprint
-import json
 
 LFM_API_KEY = "4cb074e4b8ec4ee9ad3eb37d6f7eb240"
 LFM_API_URL = "http://ws.audioscrobbler.com/2.0/"
@@ -8,7 +6,15 @@ LFM_API_URL = "http://ws.audioscrobbler.com/2.0/"
 """
     method = lfm api method
     params = dictionary (where the key is the required param in lfm api)
+
+    example call in code:
+        params = {
+            "country": "spain"
+        }
+
+        content = lfm.call("geo.getTopArtists", params)
 """
+
 def call(method, params):
     url = LFM_API_URL + "?method=" + method
 
@@ -19,12 +25,3 @@ def call(method, params):
     url += "&api_key={0}".format(LFM_API_KEY)
 
     return urllib.urlopen(url).read()
-
-if __name__ == "__main__":
-
-    params = {
-        "country": "spain"
-    }
-
-    content = call("geo.getTopArtists", params)
-    pprint.pprint(locals())
